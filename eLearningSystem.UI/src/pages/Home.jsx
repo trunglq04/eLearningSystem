6427a7942319100044e943594cfd492c0584075e
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-09-26 09:06:49 +0700

    feat(login): add login page and header footer

--- eLearningSystem.UI/src/pages/Home.jsx
+++ eLearningSystem.UI/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header></Header>
+      <div className="h-screen"></div>
+      <Footer></Footer>
+    </div>
+  );
 }
