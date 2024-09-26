9f491098f8e96fd6b46ae58f51a4e76e89d154bf
Louis <louis280104@gmail.com>
2024-09-26 23:15:34 +0700

    setup(BE): implement Onion Architecture

---
+++ Shared/DataTransferObjects/LoginUserDto.cs
@@ -0,0 +1,6 @@
+﻿
+
+namespace Shared.DataTransferObjects
+{
+    public record LoginUserDto(string UserName, string Password);
+}
---
+++ eLearningSystem.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace eLearningSystem.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                   builder
+                    .WithOrigins("https://example.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
+
+        //public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+        //    services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        //public static void ConfigureServiceManager(this IServiceCollection services) =>
+        //    services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<User, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 6;
+                o.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<RepositoryContext>()
+            .AddDefaultTokenProviders();
+        }
+    }
+}
