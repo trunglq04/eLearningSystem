60092279895eb0a3abdd471019ee170af40608f5
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-09-26 14:22:38 +0700

    feat(updateInformation): add UI update user infomation

---
+++ eLearningSystem.UI/src/pages/Profile.jsx
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { UserCircleIcon } from "@heroicons/react/20/solid";
+
+export default function Profile() {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [gender, setGender] = useState("");
+  const [dob, setDob] = useState("");
+
+  return (
+    <div>
+      <Header></Header>
+      <div className="grid grid-cols-2 py-16 px-10 h-full">
+        <div className="border-r border-gray-900/10">
+          <div className="flex justify-center align-middle h-full">
+            <div className="mt-2 flex items-center flex-col gap-x-3">
+              <UserCircleIcon
+                aria-hidden="true"
+                className="h-52 w-52 text-gray-300"
+              />
+              <button
+                type="button"
+                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                Change
+              </button>
+              <div className="mt-5">
+                <button
+                  type="button"
+                  className="rounded-md bg-rose-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-rose-500"
+                >
+                  Change Password
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <form className="px-6 py-6">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-3xl font-semibold leading-7 text-gray-900">
+              Personal Information
+            </h2>
+
+            <div className="mt-7 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="full-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Fullname
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="full-name"
+                    name="full-name"
+                    type="text"
+                    autoComplete="full-name"
+                    className="block w-full rounded-md border-0 px-3  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="gender"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Gender
+                </label>
+                <div className="mt-2">
+                  <select
+                    id="gender"
+                    name="gender"
+                    autoComplete="gender"
+                    className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Phone number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="phone-number"
+                    name="phone-number"
+                    type="text"
+                    autoComplete="phone-number"
+                    className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="col-span-full">
+                <label
+                  htmlFor="date-of-birth"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Date of Birth
+                </label>
+
+                <div className="mt-2">
+                  <input
+                    id="date-of-birth"
+                    name="date-of-birth"
+                    type="date"
+                    autoComplete="date-of-birth"
+                    className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex items-center justify-center gap-x-6">
+            <button
+              type="reset"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
