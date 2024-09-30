d4ae15f113eb521ef229061a1b575f69821c5758
Louis <louis280104@gmail.com>
2024-09-30 17:12:28 +0700

    Update(project): config services for project

--- eLearningSystem.Presentation/Controllers/AuthenticationController.cs
+++ eLearningSystem.Presentation/Controllers/AuthenticationController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
