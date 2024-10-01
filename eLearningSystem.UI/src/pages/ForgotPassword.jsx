2b9b9d0e63708eb02a85322bede89200be3d375c
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-10-01 16:51:25 +0700

    feat(forgot-password): implement forgot-password API

--- eLearningSystem.UI/src/pages/ForgotPassword.jsx
+++ eLearningSystem.UI/src/pages/ForgotPassword.jsx
@@ -17,7 +17,7 @@ export default function ForgotPassword() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.75, ease: "easeOut" }}
-          className="w-full h-full"
+          className=""
         >
           <InputEmail />
         </m.div>
--- eLearningSystem.UI/src/utils/APIServices.jsx
+++ eLearningSystem.UI/src/utils/APIServices.jsx
@@ -3,3 +3,7 @@ import client from "./AxiosClient";
 export const login = async (email, password) => {
   return client.post("/auth/login", { username: email, password });
 };
+
+export const forgotPassword = async (email) => {
+  return client.post("/password/forgot-password", { Email: email });
+};
