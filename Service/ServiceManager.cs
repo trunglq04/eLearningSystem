6abbeeb97b17799e54fc6d6069757d574656cf42
Louis <louis280104@gmail.com>
2024-10-02 22:33:35 +0700

    update(project): update service

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -12,19 +12,31 @@ namespace Service
         private readonly Lazy<IAuthenticationService> _authenticationService;
         private readonly Lazy<IEmailService> _emailService;
         private readonly Lazy<IPasswordService> _passwordService;
+        private readonly Lazy<IUserService> _userService;
+        private readonly Lazy<IMediaService> _mediaService;
 
-        public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper, IConfiguration configuration, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _authenticationService = new Lazy<IAuthenticationService>(() => 
                 new AuthenticationService(signInManager, userManager, configuration, EmailService));
+
             _emailService = new Lazy<IEmailService>(() => 
                 new SmtpEmailService(configuration));
+
             _passwordService = new Lazy<IPasswordService>(() =>
                 new PasswordService(userManager, EmailService));
+
+            _userService = new Lazy<IUserService>(() =>
+                new UserService(userManager, mapper));
+
+            _mediaService = new Lazy<IMediaService>(() =>
+                new MediaService(configuration));
         }
 
         public IAuthenticationService AuthenticationService => _authenticationService.Value;
         public IEmailService EmailService => _emailService.Value;
         public IPasswordService PasswordService => _passwordService.Value;
+        public IUserService UserService => _userService.Value;
+        public IMediaService MediaService => _mediaService.Value;
     }
 }
---
+++ Service/UserService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+
+namespace Service
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserService( UserManager<ApplicationUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+             _mapper = mapper;
+        }
+
+        public async Task<UserRequestDto> GetUser(string id)
+        {
+            var user =  await _userManager.FindByIdAsync(id);
+            var userReturn = _mapper.Map<UserRequestDto>(user);
+            return userReturn;
+        }
+
+        public Task<UserRequestDto> UpdateUser(UserRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public async Task<UserRequestDto> UpdateUser(UserRequestDto request)
+        //{
+        //    var user = _mapper.Map<ApplicationUser>(request);
+        //     user = await  _userManager.UpdateAsync(user);
+        //    var userReturn = _mapper.Map<UserRequestDto>(user);
+        //    return user;
+        //}
+    }
+}
